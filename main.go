39e957d95aa675be159950ab83635c1cfe3f0ab0
arakawa <ad.sho.loko@gmail.com>
2019-02-07 16:44:01 +0900

    impl + - * / & clear

--- main.go
+++ main.go
@@ -11,10 +11,37 @@ var inputtingNum = ""
 // 保存している値
 var accumulator = ""
 
-func doPlus(i []js.Value){
+// 選択中の演算子
+var operator = None
+
+type Operator int
+
+const (
+	Plus Operator = iota
+	Sub
+	Mul
+	Div
+	None
+)
+
+func doOperate(i []js.Value){
 	accumulator += inputtingNum
 	inputtingNum = ""
 	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", "0")
+
+	// 演算子ごとに処理する
+	switch i[0].String() {
+	case "+":
+		operator = Plus
+	case "-":
+		operator = Sub
+	case "*":
+		operator = Mul
+	case "/":
+		operator = Div
+	default:
+		operator = None
+	}
 }
 
 func doEqual(i []js.Value){
@@ -24,7 +51,20 @@ func doEqual(i []js.Value){
 
 	accumulator = ""
 
-	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", int1 + int2)
+	// 演算子ごとに計算する
+	var result int
+	switch operator{
+	case Plus:
+		result = int1 + int2
+	case Sub:
+		result = int2 - int1
+	case Mul:
+		result = int1 * int2
+	case Div:
+		result = int2 / int1
+	}
+
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", result)
 }
 
 func inputNum(i []js.Value){
@@ -32,10 +72,17 @@ func inputNum(i []js.Value){
 	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", inputtingNum)
 }
 
+func clearNum(i []js.Value){
+	inputtingNum = ""
+	accumulator = ""
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", "0")
+}
+
 func registerCallbacks() {
 	js.Global().Set("inputNum", js.NewCallback(inputNum))
-	js.Global().Set("doPlus", js.NewCallback(doPlus))
+	js.Global().Set("doOperate", js.NewCallback(doOperate))
 	js.Global().Set("doEqual", js.NewCallback(doEqual))
+	js.Global().Set("clearNum", js.NewCallback(clearNum))
 }
 
 func main(){
