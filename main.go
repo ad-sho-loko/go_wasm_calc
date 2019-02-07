a5448314cca6e3ef98d95a955fdd1f38b2450218
arakawa <ad.sho.loko@gmail.com>
2019-02-07 16:14:39 +0900

    impl add function.

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import(
+	"strconv"
 	"syscall/js"
 )
 
-func manipulateDom(i []js.Value){
-	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", "WebAssembly!")
+// 現在入力中の数字
+var inputtingNum = ""
+
+// 保存している値
+var accumulator = ""
+
+func doPlus(i []js.Value){
+	accumulator += inputtingNum
+	inputtingNum = ""
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", "0")
+}
+
+func doEqual(i []js.Value){
+	// String -> int
+	int1, _ := strconv.Atoi(inputtingNum)
+	int2, _ := strconv.Atoi(accumulator)
+
+	accumulator = ""
+
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", int1 + int2)
+}
+
+func inputNum(i []js.Value){
+	inputtingNum += i[0].String()
+	js.Global().Get("document").Call("getElementById", "result").Set("innerHTML", inputtingNum)
 }
 
 func registerCallbacks() {
-	js.Global().Set("manipulateDom", js.NewCallback(manipulateDom))
+	js.Global().Set("inputNum", js.NewCallback(inputNum))
+	js.Global().Set("doPlus", js.NewCallback(doPlus))
+	js.Global().Set("doEqual", js.NewCallback(doEqual))
 }
 
 func main(){
